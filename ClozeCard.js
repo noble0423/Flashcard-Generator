f9d2287820267df7cbdca534ab5db47b40086401
noble0423 <ryan.noble82@gmail.com>
2018-03-15 10:03:18 -0700

    basic and cloze card constructors are working

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,6 +1,17 @@
 // Create a constructor function for creating Cloze-deletion flashcards
-var ClozeCard = function(text, cloze) {
-    
+var ClozeCard = function(cloze, partial, full) {
+    this.cloze = cloze;
+    this.partial = partial;
+    this.full = full;
+    this.printCloze = function() {
+        console.log(this.cloze);
+    }
+    this.printPartial = function() {
+        console.log(this.partial);
+    };
+    this.printFull = function() {
+        console.log(this.full);
+    };
 };
 
 
--- main.js
+++ main.js
@@ -6,21 +6,18 @@ var firstPresident = new BasicCard (
     "George Washington"
 );
 
-// "Who was the first president of the United States?"
-// console.log(firstPresident.front);
-
-// "George Washington"
-// console.log(firstPresident.back);
+// firstPresident.printFront();
+// firstPresident.printBack();
 
 var firstPresidentCloze = new ClozeCard (
-    "George Washington was the first president of the United States.",
-    "George Washington"
+    "George Washington",
+    "...was the first president of the United States.",
+    "George Washington was the first president of the United States."
+   
 );
 
-// "... was the first president of the United States."
-// console.log(firstPresidentCloze.partial);
-
-// "George Washington was the first president of the United States."
-// console.log(firstPresidentCloze.fullText);
+// firstPresidentCloze.printPartial();
+// firstPresidentCloze.printFull();
+// firstPresidentCloze.printCloze();
 
 // Should throw and/or log an error be
